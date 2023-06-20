b3b163f368048d6620a4b0dcad6d8041da63a17e
Valentyn Kasyan <vkasyan@gmx.de>
2023-06-21 00:21:50 +0200

    Project Setup, Hero, Navbar & Footer

---
+++ components/CustomButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { CsutomButtonProps } from "@/types";
+import Image from "next/image";
+
+import React from "react";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CsutomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
